e15c9698f058aa010fa5ecc7838dd0612005351a
iliagerman <iliagerman@gmail.com>
2020-06-14 11:46:36 +0300

    fixed some deployment issues + added parameters replace

--- src/api/deployment-socket-api.ts
+++ src/api/deployment-socket-api.ts
@@ -1,4 +1,5 @@
 import { IDomain } from "./../interfaces/IDomain";
+import pathJoin from "path";
 import express from "express";
 import { createServer, Server } from "http";
 import socketIo from "socket.io";
@@ -9,6 +10,7 @@ import { Logger } from "../logger/logger";
 import { IPage } from "./../interfaces/IPage";
 import { spawn } from "child_process";
 import { Retryable, BackOffPolicy } from 'typescript-retry-decorator';
+import { worker } from "cluster";
 const cors = require("cors");
 
 export class DeploymentServer {
@@ -55,12 +57,21 @@ export class DeploymentServer {
       throw ("Not ready");
     }
     const totals = DeploymentServer.getTotals(domains);
+    const workingFolders: string[] = [];
+    for (let domainIndex = 0; domainIndex < domains.length; domainIndex++) {
+      for (let pageIndex = 0; pageIndex < domains[domainIndex].pages.length; pageIndex++) {
+        const page = domains[domainIndex].pages[pageIndex];
+        workingFolders.push(await this.backupWorkingFolder(page));
+      }
+    }
+    Logger.info(`Workingfolder: ${workingFolders}`);
     for (let domainIndex = 0; domainIndex < domains.length; domainIndex++) {
       for (let pageIndex = 0; pageIndex < domains[domainIndex].pages.length; pageIndex++) {
         const page = domains[domainIndex].pages[pageIndex];
         try {
           totals.curentPage = totals.curentPage + 1;
-          const exitCode = await DeploymentServer.executeScript(page, deploymentIdentifier, totals, domains[domainIndex]);
+          // await this.replaceUserParameters(workingFolder, page);
+          const exitCode = await DeploymentServer.executeScript(workingFolders[totals.curentPage - 1], page, deploymentIdentifier, totals, domains[domainIndex]);
           const deploymentMessage: IDeploymentMessage = {
             message: `Deploying: ${page.displayName} - Completed`,
             progress: totals,
@@ -72,9 +83,11 @@ export class DeploymentServer {
             deploymentMessage.message = `Deploying: ${page.displayName} - Failed`;
             deploymentMessage.final;
             DeploymentServer.socket.emit(deploymentIdentifier, deploymentMessage);
+            // await this.delay(2000);
             // return;
+          } else {
+            DeploymentServer.socket.emit(deploymentIdentifier, deploymentMessage);
           }
-          DeploymentServer.socket.emit(deploymentIdentifier, deploymentMessage);
         } catch (error) {
           Logger.error(error.message, error.stack);
           const deploymentMessage: IDeploymentMessage = {
@@ -84,8 +97,10 @@ export class DeploymentServer {
             domainName: domains[domainIndex].name
           };
           DeploymentServer.socket.emit(deploymentIdentifier, deploymentMessage);
+          // await this.delay(2000);
           return;
         }
+        // await this.delay(2000);
       }
     }
   }
@@ -106,14 +121,15 @@ export class DeploymentServer {
     return this._app;
   }
 
-  private static async backupWorkingFolder(pageName: string): Promise<string> {
+  private static async backupWorkingFolder(page: IPage): Promise<string> {
     const path = require("path");
     const timeStamp = new Date().getMilliseconds();
-    const newFolder = path.join(process.env.COMPONENTS_ROOT, `${pageName}_${timeStamp}`);
+    const newFolder = path.join(process.env.WORKING_ROOT, `${page.name}_${timeStamp}`);
     // const createFile = path.join(newFolder, "create.sh");
     const fs = require("fs-extra");
     try {
-      await fs.copy(path.join(process.env.COMPONENTS_ROOT, pageName), newFolder);
+      await fs.copy(path.join(process.env.COMPONENTS_ROOT, page.name), newFolder);
+      await this.replaceUserParameters(newFolder, page);
       // await fs.ensureFile(createFile);
       return newFolder;
     } catch (err) {
@@ -123,8 +139,53 @@ export class DeploymentServer {
     }
   }
 
-  private static replaceUserParameters(workingFolder: string, pageToExecute: IPage) {
+  private static async getFiles(path = "./"): Promise<any> {
+    try {
+      const { promises: fs } = require("fs");
+      const entries = await fs.readdir(path, { withFileTypes: true });
 
+      // Get files within the current directory and add a path key to the file objects
+      const files = entries
+        .filter((file: { isDirectory: () => any; }) => !file.isDirectory())
+        .map((file: { name: string; }) => ({ ...file, path: path + "/" + file.name }));
+
+      // Get folders within the current directory
+      const folders = entries.filter((folder: { isDirectory: () => any; }) => folder.isDirectory());
+
+      for (const folder of folders) {
+        /*
+          Add the found files within the subdirectory to the files array by calling the
+          current function itself
+        */
+        const newPath = pathJoin.join(path, folder.name);
+        files.push(...await DeploymentServer.getFiles(newPath));
+      }
+      return files;
+    } catch (error) {
+      Logger.error(error.message, error.stack);
+    }
+  }
+
+  private static async replaceUserParameters(workingFolder: string, pageToExecute: IPage) {
+    const fs = require("fs");
+    const util = require("util");
+    const readFile = util.promisify(fs.readFile);
+    const fs_writeFile = util.promisify(fs.writeFile);
+    const files = await DeploymentServer.getFiles(workingFolder);
+    for (const file of files) {
+      let content = await readFile(file.path, "utf8");
+      for (const input of pageToExecute.inputs) {
+        while (content.indexOf(`${input.serverValue}`) > 0) {
+          try {
+            content = content.replace(`${input.serverValue}`, `${input.value}`);
+          } catch (error) {
+            Logger.error(error.message, error.stack);
+          }
+        }
+        await fs_writeFile(file.path, content, "utf-8");
+      }
+    }
+    return files;
   }
 
   private static async deleteFolder(workingFolder: string) {
@@ -133,12 +194,9 @@ export class DeploymentServer {
   }
 
 
-  private static async executeScript(pageToExecute: IPage, deploymentIdentifier: string, totals: IDploymentProgress, domain: IDomain) {
+  private static async executeScript(workingFolder: string, pageToExecute: IPage, deploymentIdentifier: string, totals: IDploymentProgress, domain: IDomain) {
     try {
-      const workingFolder = await this.backupWorkingFolder(pageToExecute.name);
       const env = Object.create(process.env);
-      this.replaceUserParameters(workingFolder, pageToExecute);
-
       const deploymentProcess = spawn("sh", [`${workingFolder}/create.sh`], { env: env });
       const deploymentMessage: IDeploymentMessage = {
         message: `Deploying: ${pageToExecute.displayName}`,
@@ -162,7 +220,7 @@ export class DeploymentServer {
       }).catch((error) => {
         Logger.error(error.message, error.stack);
       });
-      await this.deleteFolder(workingFolder);
+      // await this.deleteFolder(workingFolder);
       return exitCode;
     }
     catch (error) {
